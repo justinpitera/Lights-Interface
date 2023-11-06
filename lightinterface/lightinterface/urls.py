d6f231ec50c8caf07e192717d0eec22e9807f0a2
JUSTIN PITERA <justinpitera@gmail.com>
2023-11-06 16:34:08 -0500

    Changes

--- lightinterface/lightinterface/urls.py
+++ lightinterface/lightinterface/urls.py
@@ -5,6 +5,7 @@ from lightinterface import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
+    path('bulb-statuses-json/', views.bulb_statuses_json, name='bulb_statuses_json'),
     path('turn-on-light/', views.turn_on_lights, name='turn_on_lights'),
     path('turn-off-light/', views.turn_off_lights, name='turn_off_lights'),
     path('set_color/', views.set_color, name='set_color'),
--- lightinterface/lightinterface/views.py
+++ lightinterface/lightinterface/views.py
@@ -4,12 +4,38 @@ from django.views.decorators.csrf import csrf_exempt
 from django.http import JsonResponse
 from yeelight import Bulb
 import json
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+
+
+
+from django.views.decorators.http import require_POST
+from asgiref.sync import sync_to_async  # If needed to use synchronous code in async
+import asyncio
+from yeelight.aio import AsyncBulb
+
+bulbs_ips = ["192.168.1.179", "192.168.1.175", "192.168.1.174", "192.168.1.173", "192.168.1.172"]
 
 def home(request):
     return render(request, 'home.html', {})
 
-bulbs_ips = ["192.168.1.179", "192.168.1.175", "192.168.1.174", "192.168.1.173", "192.168.1.172"]
+def bulb_statuses_json(request):
+    bulb_statuses = []
+    for ip in bulbs_ips:
+        try:
+            bulb = Bulb(ip)
+            properties = bulb.get_properties()
+            properties['readable'] = {
+                'ip': ip,
+                'power': properties['power'],
+                'bright': properties['bright'],
+                'rgb': properties['rgb'] if 'rgb' in properties else 'N/A',
+            }
+            bulb_statuses.append(properties['readable'])
+        except Exception as e:
+            bulb_statuses.append({'ip': ip, 'error': str(e)})
+    
+    return JsonResponse({'bulbs': bulb_statuses})
+
 
 
 def turn_on_lights(request):
@@ -37,8 +63,23 @@ def set_color(request):
         try:
             bulb = Bulb(ip)
             bulb.set_rgb(r, g, b)
+            bulb.set_brightness(100)
             responses[ip] = 'success'
         except Exception as e:
             responses[ip] = str(e)
     
     return JsonResponse(responses)
+
+@csrf_exempt  # Temporarily disable CSRF protection for this view
+@require_POST  # Ensure that this view can only be called with POST method
+async def set_brightness(request):
+        responses = {}
+        for ip in bulbs_ips:
+         try:
+            bulb = Bulb(ip)
+            bulb.set_brightness(100)
+            responses[ip] = 'success'
+         except Exception as e:
+            responses[ip] = str(e)
+
+            return JsonResponse(responses)
